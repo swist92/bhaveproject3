62c6e91ecac7fa0e5fc99678bbb460f3f164588a
Quackenstien <donaldhawkins422@gmail.com>
2020-07-14 14:44:14 -0700

    Auth fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Styles
 import "./App.css";
-import "./Media.css";
+
+//Authorization
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 // Pages
 import Home from "./pages/Home";
 import HivePublic from "./pages/HivePublic";
 
+Amplify.configure(awsconfig);
+
 function App() {
-	return (
-		<Router>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/HivePublic" component={HivePublic} />
-		</Router>
-	);
+  return (
+    <AmplifyAuthenticator>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/HivePublic" component={HivePublic} />
+      </Router>
+      <AmplifySignOut />
+    </AmplifyAuthenticator>
+  );
 }
 
 export default App;
