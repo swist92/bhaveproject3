6b476fbdbbb82909786379030c10a524261a80f1
Quackenstien <donaldhawkins422@gmail.com>
2020-07-14 13:58:58 -0700

    AWS Authorization

---
+++ client/src/components/Auth/SignIn.js
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// //Authentication
+// import { AmplifySignUp, AmplifySignIn } from "@aws-amplify/ui-react";
+
+// const SignIn = () => {
+//   return (
+//     <div>
+//       <AmplifySignUp
+//         slot="sign-up"
+//         usernameAlias="email"
+//         formFields={[
+//           {
+//             type: "email",
+//             label: "Custom email Label",
+//             placeholder: "custom email placeholder",
+//             required: true,
+//           },
+//           {
+//             type: "password",
+//             label: "Custom Password Label",
+//             placeholder: "custom password placeholder",
+//             required: true,
+//           },
+//         ]}
+//       />
+//       <AmplifySignIn slot="sign-in" usernameAlias="email" />
+//     </div>
+//   );
+// };
+
+// export default SignIn;
