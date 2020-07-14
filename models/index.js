df32c7bb44d7e953690f8564fa7d0f92cf94befd
Sarah Wist <sarahwist107@gmail.com>
2020-07-13 22:11:23 -0700

    clean up ignored files

--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Kitten: require("./Kitten"),
-};
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-// /api
-router.use("/api", require("./api"));
-
-module.exports = router;
