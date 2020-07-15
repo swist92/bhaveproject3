6fe4cd77c68cdbca8cff625697ac0a57e8fb1875
Quackenstien <donaldhawkins422@gmail.com>
2020-07-15 13:36:09 -0700

    unique user object

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import "./Media.css";
 
 //Authorization
 import Amplify from "aws-amplify";
+import { Auth } from "aws-amplify";
+
 import awsconfig from "./aws-exports";
 import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
@@ -17,17 +19,23 @@ import HivePublic from "./pages/HivePublic";
 Amplify.configure(awsconfig);
 
 function App() {
-	return (
-		<AmplifyAuthenticator>
-			<Router>
-				<div>
-					<Route exact path="/" component={Home} />
-					<Route path="/HivePublic" component={HivePublic} />
-				</div>
-			</Router>
-			<AmplifySignOut />
-		</AmplifyAuthenticator>
-	);
+  Auth.currentAuthenticatedUser({
+    bypassCache: true, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  })
+    .then((user) => console.log(user))
+    .catch((err) => console.log(err));
+
+  return (
+    <AmplifyAuthenticator>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/HivePublic" component={HivePublic} />
+        </div>
+      </Router>
+      <AmplifySignOut />
+    </AmplifyAuthenticator>
+  );
 }
 
 export default App;
