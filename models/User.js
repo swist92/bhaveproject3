a57dc990b5fc06f9181dd11965ffbf6a42bbd2bc
Sarah Wist <sarahwist107@gmail.com>
2020-07-15 10:07:40 -0700

    modified schema

--- models/User.js
+++ models/User.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
-  age: Number,
-  sleepy: {
-    type: Boolean,
-    default: false,
-  },
+    username: {
+        type: String,
+        required: true
+      },
+  email: {
+      type: String,
+      
+
+},
+      password: {
+          type: String,
+          required: true
+        },
 });
 
 module.exports = mongoose.model("User", UserSchema);
