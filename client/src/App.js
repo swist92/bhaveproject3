9b7f5fc4414c64a22568215201996604202e4d02
Quackenstien <donaldhawkins422@gmail.com>
2020-07-16 13:17:46 -0700

    modal built

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,13 @@ import Form from "react-bootstrap/Form";
 //Authorization
 import Amplify from "aws-amplify";
 import { Auth } from "aws-amplify";
-
-import awsconfig from "./aws-exports";
 import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
 
 // Pages
 import Home from "./pages/Home";
 import HivePublic from "./pages/HivePublic";
+import ContactModal from "../src/components/Modal/ContactModal";
 
 // Utils
 import API from "./utils/API";
@@ -46,6 +46,7 @@ function App() {
           <Route path="/HivePublic" component={HivePublic} />
         </div>
       </Router>
+
       <AmplifySignOut />
       <Navbar bg="dark" id="navb">
         <div id="mc_embed_signup">
@@ -89,69 +90,7 @@ function App() {
                 ></input>
               </div>
             </div>
-          </Form>
-        </div>
-      </Navbar>
-    </AmplifyAuthenticator>
-  );
-
-  Auth.currentAuthenticatedUser({
-    bypassCache: true, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  })
-    .then((user) => console.log(user))
-    .catch((err) => console.log(err));
-
-  //
-  return (
-    <AmplifyAuthenticator>
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/HivePublic" component={HivePublic} />
-        </div>
-      </Router>
-      <AmplifySignOut />
-      <Navbar bg="dark">
-        <Navbar.Brand href="#home">Bhave Newsletter</Navbar.Brand>
-        <div id="mc_embed_signup">
-          <Form
-            action="https://gmail.us10.list-manage.com/subscribe/post?u=61def9bff33162874e3fac869&amp;id=a4062c575d"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            className="validate"
-            target="_blank"
-            novalidate
-          >
-            <div id="mc_embed_signup_scroll">
-              <label for="mce-EMAIL">Subscribe</label>
-              <input
-                type="email"
-                value=""
-                name="EMAIL"
-                className="email"
-                id="mce-EMAIL"
-                placeholder="email address"
-                required
-              ></input>
-              <div aria-hidden="true" id="i2">
-                <input
-                  type="text"
-                  name="b_61def9bff33162874e3fac869_a4062c575d"
-                  tabindex="-1"
-                  value=""
-                ></input>
-              </div>
-              <div className="clear">
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                  className="button"
-                ></input>
-              </div>
-            </div>
+            <ContactModal />
           </Form>
         </div>
       </Navbar>
