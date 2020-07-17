a0b02406b65afbc58db5b0bbf9080b05f2fea2b7
Russell Salcido <russell.salcido@gmail.com>
2020-07-16 19:51:36 -0700

    Revert "restructure layout"

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-// Common Components
-import Nav from "./common/Nav";
-
-// React Bootstrap Components
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 //Authorization
 import Amplify from "aws-amplify";
@@ -47,16 +39,6 @@ function App() {
     .catch((err) => console.log(err));
 
   return (
-    </AmplifyAuthenticator>
-  );
-
-  Auth.currentAuthenticatedUser({
-    bypassCache: true, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  })
-    .then((user) => console.log(user))
-    .catch((err) => console.log(err));
-  
-  return (
     <AmplifyAuthenticator>
       <Router>
         <div>
@@ -64,11 +46,13 @@ function App() {
           <Route path="/HivePublic" component={HivePublic} />
         </div>
       </Router>
+
       <AmplifySignOut />
-      <Navbar bg="dark">
-        <Navbar.Brand href="#home">Bhave Newsletter</Navbar.Brand>
+      <Navbar bg="dark" id="navb">
         <div id="mc_embed_signup">
+          <Navbar.Brand href="#home">Bhave Newsletter</Navbar.Brand>
           <Form
+            inline
             action="https://gmail.us10.list-manage.com/subscribe/post?u=61def9bff33162874e3fac869&amp;id=a4062c575d"
             method="post"
             id="mc-embedded-subscribe-form"
@@ -78,7 +62,7 @@ function App() {
             novalidate
           >
             <div id="mc_embed_signup_scroll">
-              <label for="mce-EMAIL">Subscribe</label>
+              <label for="mce-EMAIL"></label>
               <input
                 type="email"
                 value=""
