92e6bf19f3902f37d58b0d6f1c426ff851fb47f3
Quackenstien <donaldhawkins422@gmail.com>
2020-07-18 16:06:25 -0700

    Submit button now populates card with predefined code.

--- client/src/components/Inspiration/Inspiration.js
+++ client/src/components/Inspiration/Inspiration.js
@@ -1,61 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-
 function Inspiration() {
-    state = {
-        image: "",
-        quote: "",
-    };
-    
-    // When the component mounts, load the next dog to be displayed
-    componentDidMount() {
-    this.loadNextInspiration();
-    }
-    
-    handleBtnClick = event => {
-    // Get the data-value of the clicked button
-    const btnType = event.target.attributes.getNamedItem("data-value").value;
-    // Clone this.state to the newState object
-    // We'll modify this object and use it to set our component's state
-    const newState = { ...this.state };
+  const [quoteState, setQuoteState] = useState({
+    quote: "",
+  });
+  //   state = {
+  //     image: "",
+  //     quote: "",
+  //   };
 
-    // Replace our component's state with newState, load the next dog image
-    this.setState(newState);
-    this.loadNextInspiration();
-    };
+  // // When the component mounts, load the next dog to be displayed
+  // componentDidMount() {
+  // this.loadNextInspiration();
+  // }
 
-    loadNextInspiration = () => {
-    API.getRandomInspiration()
-        .then(res =>
-        this.setState({
-            image: res.data.message
-        })
-        )
-        .catch(err => console.log(err));
-    };
+  // handleBtnClick = event => {
+  // // Get the data-value of the clicked button
+  // const btnType = event.target.attributes.getNamedItem("data-value").value;
+  // // Clone this.state to the newState object
+  // // We'll modify this object and use it to set our component's state
+  // const newState = { ...this.state };
 
-    render() {
-        return (
-            <>
-            <Form>
-                <Form.Group controlId="friendName">
-                    <Form.Label>Who is the person you are complimenting?</Form.Label>
-                    <Form.Control type="text" placeholder="Joan Huffelpump" />
-                    <Form.Text className="text-muted">
-                        Make someones day with our automated inspiration generator.
-                    </Form.Text>
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={props.loadNextInspiration}>
-                    Submit
-                </Button>
-            </Form>
-            <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
-            </>
-        );
-    }
+  // // Replace our component's state with newState, load the next dog image
+  // this.setState(newState);
+  // this.loadNextInspiration();
+  // };
+
+  // loadNextInspiration = () => {
+  // API.getRandomInspiration()
+  //     .then(res =>
+  //     this.setState({
+  //         image: res.data.message
+  //     })
+  //     )
+  //     .catch(err => console.log(err));
+  // };
+
+  //   render();
+  //   {
+  const handleSubmit = (e) => {
+    e.preventDefault(setQuoteState({ ...quoteState, quote: "hi" }));
+  };
+  return (
+    <>
+      <Form>
+        <Form.Group controlId="friendName">
+          <Form.Label>Who is the person you are complimenting?</Form.Label>
+          <Form.Control type="text" placeholder="ex. Joan Huffelpump" />
+          <Form.Text className="text-muted">
+            Make someones day with our automated inspiration generator.
+          </Form.Text>
+        </Form.Group>
+        <Button onClick={handleSubmit} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <Card>Quote: {quoteState.quote}</Card>
+    </>
+  );
+  //   }
 }
 
 export default Inspiration;
@@ -64,3 +70,4 @@ export default Inspiration;
 //onClick event for Random button
 //trigger axios request to Inspiration API route
 //build axios request
+// image={this.state.image} handleBtnClick={this.handleBtnClick}
