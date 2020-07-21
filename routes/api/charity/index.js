caa5222ade41c61a6b8c6eb5a6143836166a7fb5
Kathryn wade <katewade0@gmail.com>
2020-07-21 14:55:39 -0700

    charity updates

--- routes/api/charity/index.js
+++ routes/api/charity/index.js
@@ -4,9 +4,10 @@ const url = require("url");
 const { Router } = require("express");
 
 
+
 // /api/cha
 
-router.get("/charity/:search", function (req, res) {
+router.get("/", function (req, res) {
     const params = new url.URLSearchParams({
         zipCode: " ",
         searchTerm: " ",
@@ -14,17 +15,54 @@ router.get("/charity/:search", function (req, res) {
     });
     axios
         .get(
-            `http://data.orghunter.com/v1/charitysearch?user_key=${accessKey}&searchTerm=${req.params.search}&zipCode=${req.params.search}rows=5`,
+            `http://data.orghunter.com/v1/charitysearch?user_key=${accessKey}&zipCode=${req.params.search}&rows=5`,
             {
                 responseType: "json",
             }
         )
-        .then(function ({ data }) {
+        .then(function ({ search }) {
             res.json(data)
         })
         .catch(function (err) {
             console.log(err);
         });
 });
+
+// router.get("/", async function (req, res) {
+//     try {
+//       res.json(await);
+//     } catch (err) {
+//       res.status(500).end();
+//     }
+//   });
+
+// GET - /api/charity/:id
+// router.get("/:id", async function (req, res) {
+//     try {
+//       res.json(await );
+//     } catch (err) {
+//       res.status(500).end();
+//     }
+//   });
+
+//   // POST - /api/charity
+//   router.post("/", async function (req, res) {
+//     try {
+//       res.json(req.body));
+//     } catch (err) {
+//       res.status(500).end();
+//     }
+//   });
+
+//   // PUT - /api/charity/:id
+//   router.put("/:id", async function (req, res) {
+//     try {
+//       res.json(await db.charity.findByIdAndUpdate(req.params.id, req.body));
+//     } catch (err) {
+//       res.status(500).end();
+//     }
+//   });
+
+//   module.exports = router;
 
 module.exports = router;
