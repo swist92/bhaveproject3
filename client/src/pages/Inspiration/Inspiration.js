4b9b0c4d87bbacccb26503d370a164ee4a5a6aa7
Sarah Wist <sarahwist107@gmail.com>
2020-07-21 14:51:10 -0700

    modified inspiration.js

--- client/src/pages/Inspiration/Inspiration.js
+++ client/src/pages/Inspiration/Inspiration.js
@@ -4,76 +4,79 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import API from "../../utils/API";
 import Gift from "../../components/Gifts/Gift";
-
 class Inspiration extends Component {
-	state = {
-		image: "",
-	};
-
-	//   handleBtnClick = (event) => {
-	//     // Get the data-value of the clicked button
-	//     const btnType = event.target.attributes.getNamedItem("data-value").value;
-	//     // Clone this.state to the newState object
-	//     // We'll modify this object and use it to set our component's state
-	//     const newState = { ...this.state };
-
-	//     // Replace our component's state with newState, load the next dog image
-	//     this.setState(newState);
-	//     this.loadNextInspiration();
-	//   };
-
-	loadNextInspiration = (event) => {
-		event.preventDefault();
-		API.getRandomInspiration()
-			.then((res) => {
-				this.setState({
-					image: res.data[0].media,
-				});
+  state = {
+    image: "",
+    compliment: "",
+  };
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+	console.log(this.state.compliment);
+	API.createCompliment()
+		.then((res) =>{
+			this.setState({
+				compliment: this.state.compliment,
 			})
-			.catch((err) => console.log(err));
-	};
+		})
+		.catch((err) => console.log(err));
+  };
 
-	render() {
-		return (
-			<>
-				<Form>
-					<Form.Group controlId="friendName">
-						<Form.Label>Who would you like to send some love to?</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Write the name of someone you care about..."
-						/>
-					</Form.Group>
-				</Form>
-				<Button
-					variant="warning"
-					type="submit"
-					onClick={this.loadNextInspiration}
-				>
-					Random
-				</Button>
-				<Form.Text className="text-muted">
-					Make someone's day with our automated inspiration generator.
-				</Form.Text>
-				<Form.Group controlId="exampleForm.ControlTextarea1">
-					<Form.Label>Write Compliment</Form.Label>
-					<Form.Control as="textarea" rows="3" />
-				</Form.Group>
-				<Button variant="primary" type="submit" onClick={this.saveHive}>
-					Submit
-				</Button>
-				<Card onClick={this.loadNextInspiration} id="inspire-card">
-					<Card.Img variant="bottom" src={this.state.image} id="card-img" />
-				</Card>
-				<Gift></Gift>
-			</>
-		);
-	}
+  handleCompliment = (event) => {
+    this.setState({ compliment: event.target.value });
+  };
+  loadNextInspiration = (event) => {
+    event.preventDefault();
+    API.getRandomInspiration()
+      .then((res) => {
+        this.setState({
+          image: res.data[0].media,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+  render() {
+    return (
+      <>
+        <Form>
+          <Form.Group controlId="friendName">
+            <Form.Label>Who would you like to send some love to?</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Write the name of someone you care about..."
+            />
+          </Form.Group>
+        </Form>
+        <Button
+          variant="warning"
+          type="submit"
+          onClick={this.loadNextInspiration}
+        >
+          Random
+        </Button>
+        <Form.Text className="text-muted">
+          Make someone's day with our automated inspiration generator.
+        </Form.Text>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Write Compliment</Form.Label>
+          <Form.Control
+            name="compliment"
+            placeholder="EX. You look fly."
+            value={this.state.compliment}
+            onChange={this.handleCompliment}
+            as="textarea"
+            rows="3"
+          />
+        </Form.Group>
+        <Button onClick={this.handleSubmit} variant="primary">
+          Submit
+        </Button>
+        <Card onClick={this.loadNextInspiration} id="inspire-card">
+          <Card.Img variant="bottom" src={this.state.image} id="card-img" />
+        </Card>
+        <Gift></Gift>
+      </>
+    );
+  }
 }
-
 export default Inspiration;
-
-//build out component
-//onClick event for Random button
-//trigger axios request to Inspiration API route
-//build axios request
